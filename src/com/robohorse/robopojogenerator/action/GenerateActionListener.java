f0bff8501f701b28e0f1433105b2c58601f594ff
robohorse <robohorsed@gmail.com>
2016-09-24 21:49:47 +0300

    GenerateActionListener added

---
+++ src/com/robohorse/robopojogenerator/action/GenerateActionListener.java
@@ -0,0 +1,46 @@
+package com.robohorse.robopojogenerator.action;
+
+import com.robohorse.robopojogenerator.errors.JSONStructureException;
+import com.robohorse.robopojogenerator.errors.RoboPluginException;
+import com.robohorse.robopojogenerator.support.MessageService;
+import com.robohorse.robopojogenerator.view.GeneratorVew;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by vadim on 24.09.16.
+ */
+public class GenerateActionListener implements ActionListener {
+    private GuiFormEventListener eventListener;
+    private MessageService messageService = new MessageService();
+    private GeneratorVew generatorVew;
+
+    public GenerateActionListener(GeneratorVew generatorVew, GuiFormEventListener eventListener) {
+        this.generatorVew = generatorVew;
+        this.eventListener = eventListener;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JTextArea textArea = generatorVew.getTextArea();
+        try {
+            final String text = textArea.getText();
+            validateJsonContent(text);
+            eventListener.onJsonDataObtained(text);
+
+        } catch (RoboPluginException exception) {
+            messageService.onPluginExceptionHandled(exception);
+        }
+    }
+
+    private void validateJsonContent(String content) throws RoboPluginException {
+        try {
+            JSONObject jsonObject = new JSONObject(content);
+        } catch (JSONException exception) {
+            throw new JSONStructureException();
+        }
+    }
+}
--- src/com/robohorse/robopojogenerator/support/GeneratorViewCreator.java
+++ src/com/robohorse/robopojogenerator/support/GeneratorViewCreator.java
@@ -1,27 +1,17 @@
 package com.robohorse.robopojogenerator.support;
 
+import com.robohorse.robopojogenerator.action.GenerateActionListener;
 import com.robohorse.robopojogenerator.action.GuiFormEventListener;
-import com.robohorse.robopojogenerator.errors.JSONStructureException;
-import com.robohorse.robopojogenerator.errors.RoboPluginException;
 import com.robohorse.robopojogenerator.view.GeneratorVew;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Created by vadim on 24.09.16.
  */
 public class GeneratorViewCreator {
     private GuiFormEventListener eventListener;
-    private MessageService messageService;
-
-    public GeneratorViewCreator(MessageService messageService) {
-        this.messageService = messageService;
-    }
 
     public void setGuiFormEventListener(GuiFormEventListener eventListener) {
         this.eventListener = eventListener;
@@ -30,7 +20,7 @@ public class GeneratorViewCreator {
     public void showView() {
         GeneratorVew generatorVew = new GeneratorVew();
         generatorVew.getGenerateButton()
-                .addActionListener(new GenerateActionListener(generatorVew));
+                .addActionListener(new GenerateActionListener(generatorVew, eventListener));
 
         JFrame frame = new JFrame("RoboPOJOGenerator");
         frame.setContentPane(generatorVew.getRootView());
@@ -45,33 +35,5 @@ public class GeneratorViewCreator {
         int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
         int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
         frame.setLocation(x, y);
-    }
-
-    private void validateJsonContent(String content) throws RoboPluginException {
-        try {
-            JSONObject jsonObject = new JSONObject(content);
-        } catch (JSONException exception) {
-            throw new JSONStructureException();
-        }
-    }
-
-    private class GenerateActionListener implements ActionListener {
-        private GeneratorVew generatorVew;
-
-        public GenerateActionListener(GeneratorVew generatorVew) {
-            this.generatorVew = generatorVew;
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            JTextArea textArea = generatorVew.getTextArea();
-            try {
-                final String text = textArea.getText();
-                validateJsonContent(text);
-                eventListener.onJsonDataObtained(text);
-
-            } catch (RoboPluginException exception) {
-                messageService.onPluginExceptionHandled(exception);
-            }
-        }
     }
 }
