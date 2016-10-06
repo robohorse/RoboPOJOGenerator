748752baa1f3217acfe1e8bf493f2e2ad46020f1
Robohorse <robohorsed@gmail.com>
2016-10-06 22:15:55 +0300

    jsonArray identification proceesing updated

---
+++ src/main/java/com/robohorse/robopojogenerator/generator/consts/ArrayItemsTemplate.java
@@ -0,0 +1,13 @@
+package com.robohorse.robopojogenerator.generator.consts;
+
+/**
+ * Created by vadim on 06.10.16.
+ */
+public interface ArrayItemsTemplate {
+
+    String UNDEFINED_LIST = "List<Object>";
+
+    String ITEM_NAME = "%sItem";
+
+    String LIST_OF_ITEM = "List<%s>";
+}
