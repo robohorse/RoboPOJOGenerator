adb0a2a95150a956ec0fb951ac47add232739620
Robohorse <robohorsed@gmail.com>
2016-10-09 20:09:28 +0300

    classProcessor tests updated

--- src/test/java/ClassProcessorTest.java
+++ src/test/java/ClassProcessorTest.java
@@ -1,11 +1,13 @@
 import com.robohorse.robopojogenerator.generator.ClassItem;
 import com.robohorse.robopojogenerator.generator.processors.ClassProcessor;
 import com.robohorse.robopojogenerator.generator.utils.ClassGenerateHelper;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import utils.JsonReader;
 
@@ -78,6 +80,105 @@ public class ClassProcessorTest {
             if (name.equalsIgnoreCase(classItem.getClassName())) {
                 for (String key : jsonObject.keySet()) {
                     assertTrue(fields.containsKey(key));
+                }
+            } else {
+                for (String key : innerJsonObject.keySet()) {
+                    assertTrue(fields.containsKey(key));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testEmptyArrayGeneration_isCorrect() throws Exception {
+        final JSONObject jsonObject = jsonReader.read("empty_array.json");
+        final String name = "Response";
+        final Set<ClassItem> classItemSet = new HashSet<>();
+
+        when(classGenerateHelper.getClassName(name))
+                .thenReturn(name);
+
+        classProcessor.proceed(jsonObject, name, classItemSet);
+        assertTrue(classItemSet.size() == 1);
+
+        Iterator iterator = classItemSet.iterator();
+        ClassItem classItem = (ClassItem) iterator.next();
+        assertEquals(classItem.getClassName(), name);
+
+        final Map<String, String> fields = classItem.getClassFields();
+        assertNotNull(fields);
+
+        for (String key : jsonObject.keySet()) {
+            assertTrue(fields.containsKey(key));
+        }
+
+        final String targetObjectType = classItem.getClassFields().get("data");
+
+        assertEquals("List<Object>", targetObjectType);
+    }
+
+    @Test
+    public void testIntegerArrayGeneration_isCorrect() throws Exception {
+        final JSONObject jsonObject = jsonReader.read("array_with_primitive.json");
+        final String name = "Response";
+        final String targetType = "List<Integer>";
+
+        final Set<ClassItem> classItemSet = new HashSet<>();
+
+        when(classGenerateHelper.getClassName(name))
+                .thenReturn(name);
+
+        when(classGenerateHelper.resolveMajorType(Mockito.anyObject()))
+                .thenReturn(targetType);
+
+        classProcessor.proceed(jsonObject, name, classItemSet);
+        assertTrue(classItemSet.size() == 1);
+
+        Iterator iterator = classItemSet.iterator();
+        ClassItem classItem = (ClassItem) iterator.next();
+        assertEquals(classItem.getClassName(), name);
+
+        final Map<String, String> fields = classItem.getClassFields();
+        assertNotNull(fields);
+
+        for (String key : jsonObject.keySet()) {
+            assertTrue(fields.containsKey(key));
+        }
+
+        final String actualType = classItem.getClassFields().get("data");
+
+        assertEquals(targetType, actualType);
+    }
+
+    @Test
+    public void testInnerArrayObjectGeneration_isCorrect() throws Exception {
+        final JSONObject jsonObject = jsonReader.read("array_with_jsonobject.json");
+        final JSONArray innerJsonArray = jsonObject.optJSONArray("data");
+        final JSONObject innerJsonObject = innerJsonArray.getJSONObject(0);
+        final String name = "Response";
+        final String targetType = "List<DataItem>";
+
+        final Set<ClassItem> classItemSet = new HashSet<>();
+
+        when(classGenerateHelper.getClassName(name))
+                .thenReturn(name);
+        when(classGenerateHelper.resolveMajorType(Mockito.anyObject()))
+                .thenReturn(targetType);
+        when(classGenerateHelper.getClassNameWithItemPostfix(Mockito.anyString()))
+                .thenReturn(targetType);
+
+        classProcessor.proceed(jsonObject, name, classItemSet);
+        assertTrue(classItemSet.size() == 2);
+
+        for (ClassItem classItem : classItemSet) {
+            final Map<String, String> fields = classItem.getClassFields();
+            assertNotNull(fields);
+
+            if (name.equalsIgnoreCase(classItem.getClassName())) {
+                for (String key : jsonObject.keySet()) {
+                    assertTrue(fields.containsKey(key));
+                    final String actualType = classItem.getClassFields().get("data");
+                    assertEquals(targetType, actualType);
                 }
             } else {
                 for (String key : innerJsonObject.keySet()) {
