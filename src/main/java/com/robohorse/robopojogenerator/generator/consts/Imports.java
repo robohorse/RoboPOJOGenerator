58642966ba3f8106820006861464164ec9b3665b
Robohorse <robohorsed@gmail.com>
2016-10-23 14:19:02 +0300

    autoValue: gson import fixed

--- src/main/java/com/robohorse/robopojogenerator/generator/consts/Imports.java
+++ src/main/java/com/robohorse/robopojogenerator/generator/consts/Imports.java
@@ -18,7 +18,7 @@ public interface Imports {
 
     String AUTO_VALUE = "import com.google.auto.value.AutoValue;";
     String TYPED_ADAPTER = "import com.google.gson.TypeAdapter;";
-    String GSON = "import com.google.gson.Gson;";
+    String GSON_IMPORT = "import com.google.gson.Gson;";
 
     interface GSON {
         String[] IMPORTS = {GENERATED, EXPOSE, SERIALIZED_NAME};
@@ -33,6 +33,6 @@ public interface Imports {
     }
 
     interface AUTO_VALUE_GSON {
-        String[] IMPORTS = {GENERATED, SERIALIZED_NAME, AUTO_VALUE, TYPED_ADAPTER, GSON};
+        String[] IMPORTS = {GENERATED, SERIALIZED_NAME, AUTO_VALUE, TYPED_ADAPTER, GSON_IMPORT};
     }
 }
