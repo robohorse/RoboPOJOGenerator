90e1d2322a957095c0bfec01ab5e32d5cb8f2891
Vadim Shchenev <robohorsed@gmail.com>
2021-12-12 19:09:38 +0100

    [UPDATE]: Config update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,41 @@
+fun properties(key: String) = project.findProperty(key).toString()
+
+plugins {
+    id("org.jetbrains.changelog") version "1.3.1"
+}
+
 dependencies {
     implementation(project(":generator"))
     implementation(project(":core"))
 
     implementation("com.fifesoft:rsyntaxtextarea:3.1.3")
+}
+
+tasks {
+
+    patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        pluginDescription.set(
+            rootDir.resolve("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { org.jetbrains.changelog.markdownToHTML(this) }
+        )
+
+        changeNotes.set(
+            provider {
+                changelog.run {
+                    getOrNull(properties("pluginVersion")) ?: throw IllegalStateException("Please update changelog.")
+                }.toHTML()
+            }
+        )
+    }
 }
