bd8d7b19b0e2cfcadb9f8670b71f3304259a5e1b
Vadim Shchenev <robohorsed@gmail.com>
2022-08-21 15:18:54 +0200

    [Update]: build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.6.0" apply false
-    id("org.jetbrains.intellij") version "1.7.0"
+    id("org.jetbrains.intellij") version "1.8.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
 }
 
