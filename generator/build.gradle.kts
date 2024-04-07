f4cc42445d70c1b1abcd1ffec480a66ca4e93398
Vadim Shchenev <robohorsed@gmail.com>
2024-04-07 02:21:58 +0200

    Dependencies update

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     implementation(project(":core", "default"))
 
-    implementation("org.json:json:20220924")
+    implementation("org.json:json:20231013")
     implementation("commons-io:commons-io:2.11.0")
-    implementation("com.google.guava:guava:23.0")
+    implementation("com.google.guava:guava:33.1.0-jre")
 }
